ba8d3dfa84e8f2d72a877d433d622118ea312ada
Bassel-Shoeib <basselshoeib@aucegypt.edu>
2024-11-23 16:51:06 +0200

    Add files via upload

---
+++ enemy.h.cpp
@@ -0,0 +1,29 @@
+#ifndef ENEMY_H
+#define ENEMY_H
+
+#include <QGraphicsPixmapItem>
+#include <QGraphicsScene>
+#include <QTimer>
+#include <QObject>
+#include "Score.h"  
+
+class Enemy : public QObject, public QGraphicsPixmapItem {
+    Q_OBJECT
+public:
+    enum ObstacleType { Static, Moving };
+
+
+    Enemy(QGraphicsScene *scene, ObstacleType type, Score *score, int speed = 0);
+
+private:
+    QGraphicsScene *scene;
+    ObstacleType type;
+    int speed;
+    Score *score;
+
+private slots:
+    void move(); 
+
+};
+
+#endif 
