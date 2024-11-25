1ba6f57330481f352cb893b25b04048625a554b5
SeifRamy <Seifeldine@aucegypt.edu>
2024-11-25 22:28:18 +0200

    changes to main

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@ int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     QGraphicsScene* scene = new QGraphicsScene();
     QGraphicsRectItem* rect = new QGraphicsRectItem();
+    rect->setRect(0,0,100,100);
+    scene->addItem(rect);
+    QGraphicsView * view = new QGraphicsView(scene);
+    view->show();
     // Create the main window or widget
     QWidget window; // Replace QWidget with your custom main window class, e.g., MainWindow
     window.show();
