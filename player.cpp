62a0b851208f46e8b9fc2dc01bf328173afe2b5c
SeifRamy <Seifeldine@aucegypt.edu>
2024-12-08 00:46:10 +0200

    changes in level difficulty

--- player.cpp
+++ player.cpp
@@ -7,7 +7,7 @@
 #include <QGraphicsView>
 player::player(QGraphicsItem* parent, QGraphicsScene *scene1)
     : QGraphicsPixmapItem(parent), score(0), lives(3), coins(0), speed(10), hasTemporaryAbility(false), scene(scene1), isjumping(false) {
-    setPixmap(QPixmap("C:/Users/AUC/Documents/GitHub/supermario/PngItem_1478513.png"));
+    setPixmap(QPixmap("C:/Users/Dell/OneDrive/Desktop/PngItem_1478513.png"));
     setScale(0.1);
     setPos(0, 525);
     setFlag(QGraphicsItem::ItemIsFocusable);
